ea37f610b0e4bb6e63333620a68ffde7f090fd50
Jakub Kołuda <jko01@outlook.com>
2023-02-27 18:27:45 +0100

    next

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,10 +1,10 @@
-const parse = require("pg-connection-string").parse;
+import { parse } from "pg-connection-string";
 
 const { host, port, database, user, password } = parse(
   process.env.DATABASE_URL
 );
 
-module.exports = ({ env }) => ({
+export default ({ env }) => ({
   connection: {  
     client: 'postgres',
     connection: {
