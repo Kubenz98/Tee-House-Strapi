c618f9d75b71f5b683743e6d3c20a4a3bf099cdb
Jakub Kołuda <jko01@outlook.com>
2023-02-28 18:47:07 +0100

    another dep

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,23 +1,19 @@
-
-const parse = require("pg-connection-string").parse;
-
-const { host, port, database, user, password } = parse(
-  process.env.DATABASE_URL
-);
-
 module.exports = ({ env }) => ({
-  connection: {  
-    client: 'postgres',
-    connection: {
-      host,
-      port,
-      database,
-      user,
-      password,
-      ssl: {
-        rejectUnauthorized: false,
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: env("DATABASE_CLIENT", "postgres"),
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "strapi"),
+        user: env("DATABASE_USERNAME", "strapi"),
+        password: env("DATABASE_PASSWORD", "strapi"),
+        ssl: false,
       },
-    },
       debug: false,
+      options: {},
+    },
   },
 });
