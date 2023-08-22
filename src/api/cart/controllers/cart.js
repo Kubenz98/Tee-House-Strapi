e8e0867d9e04fcd457c993edc26a4c35e8cfd97d
Jakub Kołuda <jko01@outlook.com>
2023-08-22 20:19:22 +0200

    Add find to cart controller.

--- src/api/cart/controllers/cart.js
+++ src/api/cart/controllers/cart.js
@@ -6,4 +6,19 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::cart.cart');
+module.exports = createCoreController('api::cart.cart', ({strapi}) => ({
+  async find(ctx) {
+    try {
+      const user = ctx.state.user;
+      ctx.query.filters = {
+        ...(ctx.query.filters || {}),
+        user: user.id,
+      };
+      return await super.find(ctx);
+    } catch (err) {
+      console.error(err);
+      ctx.response.status = 500;
+      ctx.send({ error: "Internal server error" });
+    }
+  },
+}));
